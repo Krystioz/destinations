18e448c3125a8bd92c12cd3e3eca8fbbcda5ef53
Krystian Głuszko <gluszkokrystian@gmail.com>
2022-07-11 22:00:30 +0200

    Minor styling, changing the hero

--- src/routes/components/hero.svelte
+++ src/routes/components/hero.svelte
@@ -1,26 +1,17 @@
-<script lang="ts">
-	import Carusel from './carusel.svelte';
-</script>
-
-<div class="hero min-h-screen items-start" style="background-image: url(https://placeimg.com/1000/800/arch);">
-	<div class="hero-overlay bg-opacity-60" />
-	<div class="hero-content flex-col text-center prose-neutral text-neutral-content">
-		<div class="max-w-screen text-yellow-50">
-			<h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-			<p class="mb-5">
+<div class="hero items-start min-h-screen bg-base-200">
+	<div class="hero-content flex-col lg:flex-row">
+		<img
+			src="https://placeimg.com/1000/400/arch"
+			class="max-w-xl w-full rounded-lg shadow-2xl"
+			alt="nature"
+		/>
+		<div class="prose">
+			<h1 class="">Box Office News!</h1>
+			<p class="">
 				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-				quasi. In deleniti eaque aut repudiandae et a id Lorem, ipsum dolor sit amet consectetur
-				adipisicing elit. Necessitatibus est, earum vel et dolor quam, saepe quidem ut modi,
-				excepturi ullam impedit. Necessitatibus ad consequuntur facilis maxime dolorum numquam
-				soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt unde similique
-				non. Ipsa nihil aspernatur, recusandae non quia cumque qui natus?Lorem ipsum dolor sit amet
-				consectetur, adipisicing elit. Reprehenderit cumque laboriosam porro libero perspiciatis
-				aliquam impedit magni. Nisi corrupti ea tempora
-				<a href="/" class="link">ABC</a>
+				quasi. In deleniti eaque aut repudiandae et a id nisi.
 			</p>
 			<button class="btn btn-primary">Get Started</button>
 		</div>
 	</div>
 </div>
-
-
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,15 @@
 <script>
 	import Hero from './components/hero.svelte';
-    import Feature from './components/feature.svelte';
+	import Feature from './components/feature.svelte';
 </script>
 
-
 <svelte:head>
-  <title>Welcome</title>
+	<title>Welcome</title>
 </svelte:head>
 
-<Hero/>
-<Feature/>
-<h2>asdasdas
-  <h3>asjlndnaljsdlnasdlnj</h3>
+<Hero />
+<Feature />
+<h2>
+	asdasdas
+	<h3>asjlndnaljsdlnasdlnj</h3>
 </h2>
