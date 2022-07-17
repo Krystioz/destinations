31f2c1372c1ef2ded8df886ed4be111aea216493
Krystian Głuszko <gluszkokrystian@gmail.com>
2022-07-17 16:25:53 +0200

    styling, experimenting with fetching and database

--- src/routes/components/header.svelte
+++ src/routes/components/header.svelte
@@ -131,31 +131,37 @@
 			<!-- This example requires Tailwind CSS v2.0+ -->
 			<div class="relative z-10 inline-block text-left">
 				<div>
-					<button
-						data-tip="change theme"
-						on:click={checkDropdownStatus}
-						type="button"
-						class="tooltip tooltip-left inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
-						id="menu-button"
-						aria-expanded="true"
-						aria-haspopup="true"
-					>
-						Themes
-						<!-- Heroicon name: solid/chevron-down -->
-						<svg
-							class="-mr-1 ml-2 h-5 w-5"
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-							aria-hidden="true"
+					<div class="indicator">
+						<span
+							data-tip="change theme"
+							class="badge indicator-item tooltip tooltip-left badge-secondary"
+							>?</span
 						>
-							<path
-								fill-rule="evenodd"
-								d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-								clip-rule="evenodd"
-							/>
-						</svg>
-					</button>
+						<button
+							on:click={checkDropdownStatus}
+							type="button"
+							class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
+							id="menu-button"
+							aria-expanded="true"
+							aria-haspopup="true"
+						>
+							Themes
+							<!-- Heroicon name: solid/chevron-down -->
+							<svg
+								class="-mr-1 ml-2 h-5 w-5"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								aria-hidden="true"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</button>
+					</div>
 				</div>
 
 				{#if hideSelector}<div
--- src/routes/places.svelte
+++ src/routes/places.svelte
@@ -1,54 +1,158 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { onDestroy } from 'svelte';
-	const fetchPlaces = async () => {
+	import { initializeApp } from 'firebase/app';
+	import { getDatabase, ref, onValue, get } from 'firebase/database';
+	import { getAnalytics, logEvent } from 'firebase/analytics';
+
+	const firebaseConfig = {
+		apiKey: 'AIzaSyCsEUT-SULj3zJGSNxWGxnPCuOB8EXh4MQ',
+		authDomain: 'destinations-17365.firebaseapp.com',
+		projectId: 'destinations-17365',
+		storageBucket: 'destinations-17365.appspot.com',
+		messagingSenderId: '390176124792',
+		appId: '1:390176124792:web:2ba76a77eeed208014a28d',
+		measurementId: 'G-XT8Q3MCF6S',
+		databaseURL: 'https://destinations-17365-default-rtdb.firebaseio.com'
+	};
+
+	const app = initializeApp(firebaseConfig);
+	// const analytics = getAnalytics(app);
+	const db = getDatabase(app);
+	let gotdata = false;
+
+	const countries = ref(db, '/Countries');
+	onValue(countries, (snapshot) => {
+		const data = snapshot.val();
+	});
+
+	let Countries;
+	get(countries).then((snapshot) => {
+		Countries = snapshot.val();
+		gotdata = true;
+	});
+
+	// historical%2Cnature%2Cmonuments%2Czoos
+	const fetchPlaces = async (
+		longitude: number,
+		latitude: number,
+		categories: string,
+		rate: number,
+		radius: number,
+		limit: number
+	) => {
 		var response = await fetch(
-			'https://api.opentripmap.com/0.1/en/places/radius?radius=100000&lon=21.012230&lat=52.229675&limit=12&apikey=5ae2e3f221c38a28845f05b606b3b76d805d8dd89180dcbbbbcbdbf8'
+			`https://api.opentripmap.com/0.1/en/places/radius?radius=${radius}&lon=${longitude}&lat=${latitude}&limit=${limit}&rate=${rate}&kinds=${categories}&apikey=5ae2e3f221c38a28845f05b606b3b76d805d8dd89180dcbbbbcbdbf8`
 		);
 
 		var result = await response.json();
-		console.log(result);
 		return result;
 	};
 
-
 	let promisePlaces = fetchPlaces();
-	let mounted: boolean;
 
+	function getPlaces() {
+		let promisePlaces = fetchPlaces();
+		return promisePlaces;
+		console.log(Countries);
+	}
+
+	//  "xid": "W202593695",
+	// "name": "Museu del Tabac",
+	// "dist": 89.15736524,
+	// "rate": 3,
+	// "osm": "way/202593695",
+	// "wikidata": "Q2912611",
+	// "kinds": "cultural,museums,interesting_places,other_museums",
+	// "point": {
+	//   "lon": 1.4915251731872559,
+	//   "lat": 42.46450424194336
+	// //saving places to my db
+	// N376290602
+	// 	function writePlacesData(xid, cords,id,properties){
+	// let c=""
+	// 	}
 </script>
 
-<section class=" m-auto mb-10 flex flex-col items-center justify-between gap-1">
-	<div class=" prose mt-12 text-center">
+<section class="mb-10 flex flex-col justify-between gap-1">
+	<div class="prose mx-auto mt-12 flex flex-col items-center text-center">
 		<h2>Welcome to the destination searcher !</h2>
 		<h3>I hope you will have fun</h3>
 	</div>
 	<div class="divider">Look around</div>
 
-	{#await promisePlaces}
+	<nav class="mx-10 mb-8 flex flex-row items-center justify-center gap-9">
+		<select class="select select-bordered select-sm w-44 max-w-xs">
+			<option disabled selected>Państwa</option>
+			{#if gotdata}
+				{#each Object.values(Countries) as country}
+					<option>{country.translations.pol.official}</option>
+				{/each}
+			{:else}
+				<option>Loading your data</option>
+			{/if}
+		</select>
+		<button on:click={getPlaces} class="btn btn-success">Search</button>
+		<!-- The button to open modal -->
+	</nav>
+
+
+	<label for="my-modal-3" class="modal-button btn w-24">open modal</label>
+	<!-- Put this part before </body> tag -->
+	<input type="checkbox" id="my-modal-3" class="modal-toggle" />
+	<div class="modal">
+		<div class="modal-box relative h-80">
+			<label for="my-modal-3" class="btn btn-circle btn-sm absolute right-2 top-2">✕</label>
+			<h3 class="text-lg font-bold">Wybierz państwo</h3>
+			{#if gotdata}
+				{#each Object.values(Countries) as country}
+					<a class="text-xs link m-2">{country.translations.pol.official}</a>
+				{/each}
+			{:else}
+				<p>Loading your data</p>
+			{/if}
+		</div>
+	</div>
+
+	<div class="collapse bg-slate-50">
+  <input type="checkbox" /> 
+  <div class="collapse-title text-xl font-medium">
+    Click me to show/hide content
+  </div>
+  <div class="collapse-content"> 
+    <p>tabindex="0" attribute is necessary to make the div focusable</p>
+  </div>
+</div>
+
+	<!-- {#await promisePlaces}
 		<h3>Loading your places</h3>
 	{:then places}
-			<div class="flex w-auto flex-row flex-wrap justify-center gap-4 mb-10">
-				{#each places.features as place}
-					<div class="card-compact card w-96 bg-base-100 shadow-xl">
-						<figure>
-							<img src="https://placeimg.com/600/400/nature" alt="nature images" />
-						</figure>
-						<div class="card-body">
-							<h2 class="card-title">{place.id}</h2>
-							{#if place.properties.name == ''}
-								<p>Brak opisu</p>
-							{:else}
-								<p>{place.properties.name}</p>
-							{/if}
+		<div class="flex w-auto mx-2 flex-row flex-wrap align-middle gap-4 mb-10">
+			{#each places.features as place}
+				<div class="card-compact card w-96 bg-base-100 shadow-xl">
+					<figure>
+						<img
+							class="object-fill w-96"
+							src="http://placeimg.com/640/480/any"
+							alt="nature images"
+						/>
+					</figure>
+					<div class="card-body">
+						<h2 class="card-title">{place.id}</h2>
+						{#if place.properties.name == ''}
+							<p>Brak opisu</p>
+						{:else}
+							<p>{place.properties.name}</p>
+						{/if}
 
-							<div class="card-actions justify-end">
-								<button class="btn-xs rounded-lg btn-primary">Zobacz więcej</button>
-							</div>
+						<div class="card-actions justify-end">
+							<button class="btn-xs rounded-lg btn-primary">Zobacz więcej</button>
 						</div>
 					</div>
-				{/each}
-			</div>
+				</div>
+			{/each}
+		</div>
 	{:catch Error}
 		<h2 class="text-center">error loading data : {Error.message}</h2>
-	{/await}
+	{/await} -->
 </section>
