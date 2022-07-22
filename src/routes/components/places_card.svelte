0dd5829b76a4c5c11ace22193883dab05fa83091
Krystian Głuszko <gluszkokrystian@gmail.com>
2022-07-22 23:12:14 +0200

    card spinner on load

--- src/routes/components/places_card.svelte
+++ src/routes/components/places_card.svelte
@@ -4,20 +4,25 @@
 	export let placeLong: Number;
 </script>
 
-<div class="card card-compact w-96 bg-base-100 shadow-xl">
+<div class="card-compact card w-96 bg-base-100 shadow-xl">
 	<figure>
-		<img class="w-96 object-fill" src="http://placeimg.com/640/480/any" alt="nature images" />
+		<img class="w-96 object-cover" src="http://placeimg.com/640/480/any" alt="nature images" />
 	</figure>
 	<div class="card-body">
-		<h2 class="card-title">{placeName}</h2>
+		<h2 class="card-title">{placeName}
+        <div class="badge inline badge-secondary">NEW</div></h2>
+        
 		{#if placeName == ''}
 			<p>Brak opisu</p>
 		{:else}
 			<p>{placeName}</p>
 		{/if}
+           
 
 		<div class="card-actions justify-end">
-			<button class="btn-primary btn-xs rounded-lg">Zobacz więcej</button>
+			<button class="btn-primary btn-xs rounded-lg"
+				><a href="/">Zobacz więcej</a>
+			</button>
 			<button class="btn-primary btn-xs rounded-lg text-center"
 				><a
 					class="p-3"
